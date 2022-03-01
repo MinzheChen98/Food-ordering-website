6ff91411c5013e45c1536f3b165f1472ddd94bf6
minzhe Chen <m433chen@uwaterloo.ca>
2022-03-01 15:18:35 +0800

    fe

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -2,16 +2,14 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./containers/Home";
 import Login from "./containers/Login";
+import SignUp from './containers/sign-up/index'
 
 export default function CustomRoutes() {
   return (
     <Routes>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route exact path="/login">
-        <Login />
-      </Route>
+      <Route exact path="/" element={<Home />} />
+      <Route exact path="/login" element={<Login />} />
+      <Route exact path="/signup" element={<SignUp />} />
     </Routes>
   );
 }
